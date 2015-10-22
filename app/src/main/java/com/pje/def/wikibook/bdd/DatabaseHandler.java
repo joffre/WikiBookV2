5cbc7dd1a07e2d11efb1b4d066d7437efc19b26f
gescobedo <joey59290@gmail.com>
2015-10-22 16:59:51 +0200

    FilterDetails proper 2

--- app/src/main/java/com/pje/def/wikibook/bdd/DatabaseHandler.java
+++ app/src/main/java/com/pje/def/wikibook/bdd/DatabaseHandler.java
@@ -5,46 +5,73 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+
 /**
  * Created by Geoffrey on 22/10/2015.
  */
-public class DatabaseHandler extends SQLiteOpenHelper{
+public class DatabaseHandler extends OrmLiteSqliteOpenHelper {
 
-    public static final String TABLE_BOOK = "book";
-    public static final String COLUMN_ISBN = "isbn";
-    public static final String COLUMN_TITLE = "title";
-    public static final String COLUMN_AUHTOR = "author";
-    public static final String COLUMN_YEAR = "year";
-    public static final String COLUMN_GENRE = "genre";
-    public static final String COLUMN_DESCRIPTION = "description";
-    private static final String DATABASE_NAME = "book.db";
+    private static final String DATABASE_NAME = "wikibookdir.db";
     private static final int DATABASE_VERSION = 1;
-    // Commande sql pour la création de la base de données
-    private static final String DATABASE_CREATE = "create table "
-            + TABLE_BOOK + "("
-            + COLUMN_ISBN + " text primary key "
-            + COLUMN_TITLE + " text, "
-            + COLUMN_AUHTOR + " text, "
-            + COLUMN_YEAR + " text, "
-            + COLUMN_GENRE + " text, "
-            + COLUMN_DESCRIPTION + " text, "
-            + ");";
+
+    private Dao<BookDetails, Integer> bookDao;
+    private Dao<FilterDetails, Integer> filterDao;
 
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(DATABASE_CREATE);
+    public void onCreate(SQLiteDatabase sqliteDatabase, ConnectionSource connectionSource) {
+        try {
+
+            // Create tables. This onCreate() method will be invoked only once of the application life time i.e. the first time when the application starts.
+            TableUtils.createTable(connectionSource, BookDetails.class);
+            TableUtils.createTable(connectionSource, FilterDetails.class);
+
+        } catch (SQLException e) {
+            Log.e(DatabaseHandler.class.getName(), "Unable to create databases", e);
+        }
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
-        Log.w(DatabaseHandler.class.getName(),
-                "Upgrading database from version " + oldVersion + " to "
-                        + newVersion + ", which will destroy all old data");
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_BOOK);
-        onCreate(sqLiteDatabase);
+    public void onUpgrade(SQLiteDatabase sqliteDatabase, ConnectionSource connectionSource, int oldVer, int newVer) {
+        try {
+
+            // In case of change in database of next version of application, please increase the value of DATABASE_VERSION variable, then this method will be invoked
+            //automatically. Developer needs to handle the upgrade logic here, i.e. create a new table or a new column to an existing table, take the backups of the
+            // existing database etc.
+
+            TableUtils.dropTable(connectionSource, BookDetails.class, true);
+            TableUtils.dropTable(connectionSource, FilterDetails.class, true);
+            onCreate(sqliteDatabase, connectionSource);
+
+        } catch (SQLException e) {
+            Log.e(DatabaseHandler.class.getName(), "Unable to upgrade database from version " + oldVer + " to new "
+                    + newVer, e);
+        }
+    }
+
+    // Create the getDao methods of all database tables to access those from android code.
+    // Insert, delete, read, update everything will be happened through DAOs
+
+    public Dao<BookDetails, Integer> getBookDao() throws SQLException {
+        if (bookDao == null) {
+            bookDao = getDao(BookDetails.class);
+        }
+        return bookDao;
+    }
+
+    public Dao<FilterDetails, Integer> getFilterDao() throws SQLException {
+        if (filterDao == null) {
+            filterDao = getDao(FilterDetails.class);
+        }
+        return filterDao;
     }
 }
